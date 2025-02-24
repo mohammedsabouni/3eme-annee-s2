831a80ca6b128d58a446dfd78813e414aeaa1b2e
mohammedsabouni <mohammed.sabouni.2002@gmail.com>
2025-02-24 15:59:49 +0000

    constructeur destructeur point

--- c++/pojet1/Point.cpp
+++ c++/pojet1/Point.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include"Point.hpp"
+#include<string.h>
 
 void Point::saisie(){
     std::cout << "entrez x , y et nom" << std::endl;
@@ -8,4 +9,37 @@ void Point::saisie(){
 
 void Point::affiche(){
     std::cout << this->nom << "("<< this->x << " , "<< this->y << ")" << std::endl;
+}
+
+int Point::nbrpt = 0;
+Point::Point(){
+    x = 0.0;
+    y = 0.0;
+    nom = new char[20];
+    strcpy(nom, "AA");
+    Point::nbrpt++;
+    std::cout << "constructeur par default" << std::endl;
+}
+
+Point::Point(const float & n, const char *  c){
+    x = n;
+    y = n;
+    nom = new char [strlen(c)+1];
+    strcpy(nom,c);
+    Point::nbrpt++;
+
+}
+
+Point::Point(const float & x_, const float & y_, const char *  nom_){
+    x = x_;
+    y = y_;
+    nom = new char[strlen(nom_)+1];
+    strcpy(nom, nom_);
+    Point::nbrpt++;
+
+}
+
+Point::~Point(){
+    delete nom;
+    std::cout << "Destructeur" << std::endl; 
 }
--- c++/pojet1/main.cpp
+++ c++/pojet1/main.cpp
@@ -2,12 +2,16 @@
 #include"Point.hpp"
 
 int main(){
-    Point A, *B;
-    B = new Point;
-    A.saisie();
-    B->saisie();
+    Point A, *B, C(2,3,"ABC");
+    B = new Point(1.3, "BBB");
     A.affiche();
+    // A.saisie();
+    // B->saisie();
+    // A.affiche();
     B->affiche();
+    C.affiche();
+    delete B;
+    std::cout << "*********" << std::endl;
 
     return 0;
 }
