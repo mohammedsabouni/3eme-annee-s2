f6cf708db956a97c21357ccc5cb2fb0da162cffa
mohammedsabouni <mohammed.sabouni.2002@gmail.com>
2025-03-17 23:34:54 +0000

    ajout operaeurs

--- c++/pojet1/Point.hpp
+++ c++/pojet1/Point.hpp
@@ -13,11 +13,15 @@
             Point(const float &, const float &, const char * );
             Point(const Point &);//constructeur de recopie
             ~Point();//destructeur on peut avoir un seule
-            Point & operator = (const Point &);
+            Point & operator= (const Point &);
             void saisie();
             void affiche() const;
             bool compare(const Point &) const;
-               
+            Point & operator+ (const Point &);
+            Point & operator* (const float & );
+            friend Point & operator* (const float &, const Point &);
+            friend std::istream & operator>> (std::istream &, Point &);  
+            friend std::ostream & operator<< (std::ostream &,const Point &);
     };
        
 #endif
