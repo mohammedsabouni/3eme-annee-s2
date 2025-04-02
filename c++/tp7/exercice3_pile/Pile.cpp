b5fb285b0e9bb9da7fa0c214f43fa9b1aac50e35
mohammedsabouni <mohammed.sabouni.2002@gmail.com>
2025-04-02 17:14:45 +0000

    add operators Pile

--- c++/tp7/exercice3_pile/Pile.cpp
+++ c++/tp7/exercice3_pile/Pile.cpp
@@ -7,6 +7,17 @@ Pile::Pile(int t){
     taille = 0;
 }
 
+Pile::Pile(Pile& p){
+    dim = p.dim;
+    taille = p.taille;
+    adr = new int[dim];
+    for (int i = 0; i < taille; i++)
+    {
+        *(adr+i) = *(p.adr+i);
+    }
+    
+}
+
 Pile::~Pile(){
     delete []adr;
 }
@@ -56,3 +67,82 @@ bool Pile::pleine() const {
 int Pile::donnetaille() const {
     return taille;
 }
+
+Pile& Pile::operator=(const Pile& P){
+    dim = P.dim;
+    taille = P.taille;
+    delete [] adr;
+    adr = new int[dim];
+    for (int i = 0; i < taille; i++)
+    {
+        *(adr+i) = *(P.adr + i);
+    }
+
+    return *(this);
+}
+
+bool Pile::operator==(const Pile& P)const{
+    if(taille == P.taille){
+        for (int i = 0; i < taille; i++)
+        {
+            if(*(adr) != *(P.adr))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    else return false;
+    
+}
+
+void Pile::afficher()const
+{
+    std::cout << "dim : " << dim << std::endl;
+    std::cout << "taille : " << taille << std::endl;
+    for (int i = 0; i < taille; i++)
+    {
+        std::cout << *(adr+i) << " |\t";
+    }
+    std::cout << std::endl;
+}
+
+int Pile::operator[](int p)const{
+    if(p > taille || p < 0){
+        std::cout<<" cet element n existe pas" << std::endl;
+    }
+    else{
+        return *(adr+p);
+    }
+}
+
+Pile& Pile::operator+(Pile& p)const{
+    if (taille != p.taille)
+    {
+        std::cout << "tailles differentes" << std::endl;
+    }
+    else
+    {
+        static Pile res(p);
+        for (int i = 0; i < taille; i++)
+        {
+            *(res.adr+i) += *(adr+i);
+        }
+        return res;
+        
+    }    
+}
+
+std::ostream& operator<<(std::ostream& COUT,const Pile& p){
+    for (int i = 0; i < p.taille; i++)
+    {
+        COUT << *(p.adr + i) << " |\t";
+    }
+    COUT << std::endl;
+    return COUT;
+    
+}
+
+
+
+
--- c++/tp7/exercice3_pile/Pile.hpp
+++ c++/tp7/exercice3_pile/Pile.hpp
@@ -7,11 +7,19 @@ class Pile {
         int *adr; 
     public : 
         Pile(int t = 10); //Constructeur qui construit une pile de dimension t (10 par defaut) 
+        Pile(Pile&); //recopie
         ~Pile(); //Destructeur 
         void empile(int); //empile n en haut de la pile 
         void depile(); //depile le sommet de la pile 
         bool vide() const; //teste si la pile est vide 
         bool pleine() const; //teste si la pile est pleine 
         int donnetaille() const; //renvoie la taille de la pile 
+        void afficher()const;
+
+        Pile& operator=(const Pile&);
+        bool operator==(const Pile&)const;
+        int operator[](int)const;
+        Pile& operator+(Pile&)const;
+        friend std::ostream& operator<<(std::ostream&,const Pile&);
 }; 
 #endif
